8aecde2285d278ca5e07fae7a4c9a0e42de712ed
rallaakhil <r.akhil.9640@gmail.com>
2018-10-31 15:18:33 +0530

    Variable declarations added

--- src/ast.cpp
+++ src/ast.cpp
@@ -13,9 +13,10 @@ int  AST::accept(Visitor *v){v->visit(this);}
 // {	public:
   	///declarlist *de;
   	// statlist *se;
-Program::Program(Field_declarations *fields_declarations)
+Program::Program(Field_declarations *field_declarations, Method_declarations *method_declarations)
 {
-  this->fields = fields_declarations;
+  this->fields = field_declarations;
+  this->methods = method_declarations;
 }
  //  	Program(class fieldDeclarations *fields, class methodDeclarations *methods) {
 	//     this->methods = methods;
@@ -72,6 +73,79 @@ Var_declaration::Var_declaration(string name, unsigned int array_size) {
 }
 Var_declaration::Var_declaration(string name) {
     this->declType = variableType::Normal;
+    this->name = name;
+}
+
+
+
+void Method_declarations::Push_back(Method_declaration *method_declaration)
+{
+  method_declaration_list.push_back(method_declaration);
+}
+
+
+Method_declaration::Method_declaration(string method_type, string name, Method_args_declarations *method_args_declarations, Block *block)
+{
+  this->method_type = method_type;
+  this->name = name;
+  this->method_args_declarations = method_args_declarations;
+  this->block = block;
+}
+
+
+
+
+void Method_args_declarations::Push_back(Method_args_declaration *method_args_declaration)
+{
+  this->method_args_declaration_list.push_back(method_args_declaration);
+}
+
+
+
+
+Method_args_declaration::Method_args_declaration(string arg_type, string name)
+{
+  this->arg_type = arg_type;
+  this->name = name;
+}
+
+
+
+Block::Block(Field_method_declarations *field_method_declarations)
+{
+  this->field_method_declarations = field_method_declarations;
+}
+
+
+
+
+void Field_method_declarations::Push_back(Field_method_declaration *field_method_declaration)
+{
+  declaration_list.push_back(field_method_declaration);
+}
+
+
+
+
+Field_method_declaration::Field_method_declaration(string dataType, Var_method_declarations *var_method_declarations)
+{
+  this->dataType = dataType;
+  this->var_list = var_method_declarations->getVarsList();
+}
+
+
+
+void Var_method_declarations::Push_back(Var_method_declaration *var_method_declaration) {
+    var_declaration_list.push_back(var_method_declaration);
+}
+vector<Var_method_declaration *> Var_method_declarations::getVarsList() {
+    return var_declaration_list;
+}
+
+
+
+
+Var_method_declaration::Var_method_declaration(string name) {
     this->name = name;
 }
 
--- src/ast.h
+++ src/ast.h
@@ -8,6 +8,15 @@ class Field_declarations;
 class Field_declaration;
 class Var_declarations;
 class Var_declaration;
+class Method_declarations;
+class Method_declaration;
+class Method_args_declarations;
+class Method_args_declaration;
+class Block;
+class Field_method_declarations;
+class Field_method_declaration;
+class Var_method_declarations;
+class Var_method_declaration;
 
 enum variableType{
     Array = 1, Normal = 2
@@ -21,6 +30,15 @@ class Visitor
     virtual int visit(Field_declaration *vis){}
     // virtual int visit(Var_declarations *vis){}
     virtual int visit(Var_declaration *vis){}
+    virtual int visit(Method_declarations *vis){}
+    virtual int visit(Method_declaration *vis){}
+    virtual int visit(Method_args_declarations *vis){}
+    virtual int visit(Method_args_declaration *vis){}
+    virtual int visit(Block *vis){}
+    virtual int visit(Field_method_declarations *vis){}
+    virtual int visit(Field_method_declaration *vis){}
+    // virtual int visit(Var_method_declarations *vis){}
+    virtual int visit(Var_method_declaration *vis){}
 		// virtual int visit(statlist *sta){}
 		// virtual int visit(stat *sta){}
 		// virtual int visit(bl *sta){}
@@ -56,7 +74,8 @@ class Program:public AST
   	///declarlist *de;
   	// statlist *se;
   		class Field_declarations *fields;
-  		Program(class Field_declarations *);
+      class Method_declarations *methods;
+  		Program(class Field_declarations *, class Method_declarations *);
 
  //  	Program(class fieldDeclarations *fields, class methodDeclarations *methods) {
 	//     this->methods = methods;
@@ -111,11 +130,94 @@ class Var_declaration:public AST
 		unsigned int length;
 		string name;
 		Var_declaration(string, unsigned int);
-    	explicit Var_declaration(string);
-    	bool isArray() { return (declType == variableType::Array); }
-    	unsigned int getLength() {return length;}
+  	explicit Var_declaration(string);
+  	bool isArray() { return (declType == variableType::Array); }
+  	unsigned int getLength() {return length;}
 	virtual int accept(Visitor *v){v->visit(this);}
 };	
+
+class Method_declarations:public AST
+{
+  public:
+    vector<class Method_declaration *> method_declaration_list;
+    Method_declarations() = default;
+    void Push_back(class Method_declaration *);
+  virtual int accept(Visitor *v){v->visit(this);}
+};
+
+class Method_declaration:public AST
+{
+  public:
+    string method_type;
+    string name;
+    class Method_args_declarations *method_args_declarations;
+    class Block *block;
+    Method_declaration(string, string, Method_args_declarations *, Block *);
+  virtual int accept(Visitor *v){v->visit(this);}
+};
+
+class Method_args_declarations:public AST
+{
+  public:
+    vector<class Method_args_declaration *> method_args_declaration_list;
+    Method_args_declarations() = default;
+    void Push_back(class Method_args_declaration *);
+  virtual int accept(Visitor *v){v->visit(this);}
+};
+
+class Method_args_declaration:public AST
+{
+  public:
+    string arg_type;
+    string name;
+    Method_args_declaration(string,string);
+  virtual int accept(Visitor *v){v->visit(this);}
+};
+
+class Block:public AST
+{
+  public:
+    class Field_method_declarations *field_method_declarations;
+    Block(Field_method_declarations *);
+  virtual int accept(Visitor *v){v->visit(this);}
+};
+
+class Field_method_declarations:public AST
+{
+  public:
+    vector<class Field_method_declaration *> declaration_list;
+    Field_method_declarations() = default;
+    void Push_back(class Field_method_declaration *);
+  virtual int accept(Visitor *v){v->visit(this);} 
+};
+
+class Field_method_declaration:public AST
+{
+  public:
+    string dataType;
+    vector<Var_method_declaration *> var_list;
+    Field_method_declaration(string, Var_method_declarations *);
+  virtual int accept(Visitor *v){v->visit(this);}
+};
+
+class Var_method_declarations:public AST
+{
+  public:
+    vector<Var_method_declaration *> var_declaration_list;
+    Var_method_declarations() = default;
+    void Push_back(Var_method_declaration *);
+    vector<Var_method_declaration *> getVarsList();
+  virtual int accept(Visitor *v){v->visit(this);}
+};
+
+class Var_method_declaration:public AST
+{
+  public:   
+    string name;
+    Var_method_declaration(string);
+  virtual int accept(Visitor *v){v->visit(this);}
+};
+
 
 // class Type:public AST
 // {
@@ -144,6 +246,8 @@ public:
     	cout<<"entered Program\n";
     	if(vis->fields!=NULL)
     		vis->fields->accept(this);
+      if(vis->methods!=NULL)
+        vis->methods->accept(this);
     }
     int visit(Field_declarations *vis)
     {
@@ -178,5 +282,72 @@ public:
 			cout<<"It's a Normal variable\n";
     		cout<<vis->name<<"\n";
     	}
+    }
+
+    int visit(Method_declarations *vis)
+    {
+      cout<<"Entered Method Declarations\n";
+      for(int i=0;i<vis->method_declaration_list.size();i++)
+      {
+        vis->method_declaration_list[i]->accept(this);
+      }
+    }
+
+    int visit(Method_declaration *vis)
+    {
+      cout<<"Entered Method Declaration\n";
+      cout<<"method type "<<vis->method_type<<" method name "<<vis->name<<"\n";
+      if(vis->method_args_declarations!=NULL)
+      {
+        vis->method_args_declarations->accept(this);
+      }
+      vis->block->accept(this);
+    }
+
+    int visit(Method_args_declarations *vis)
+    {
+      cout<<"Entered Arguments of method\n";
+      for(int i=0;i<vis->method_args_declaration_list.size();i++)
+      {
+        vis->method_args_declaration_list[i]->accept(this);
+      }
+    }
+
+    int visit(Method_args_declaration *vis)
+    {
+      cout<<"Argument type "<<vis->arg_type<<" argument name "<<vis->name<<endl;
+    }
+
+    int visit(Block *vis)
+    {
+      cout<<"Enterend Block\n";
+      if(vis->field_method_declarations!=NULL)
+        vis->field_method_declarations->accept(this);
+    }
+
+    int visit(Field_method_declarations *vis)
+    {
+      cout<<"Entered Field_method_declarations\n";
+      for(int i=0;i<vis->declaration_list.size();i++)
+        vis->declaration_list[i]->accept(this);
+    }
+
+    int visit(Field_method_declaration *vis)
+    {
+      cout<<"Entered Field_method_declaration\n";
+      cout<<"Variable Type "<<vis->dataType<<"\n";
+      for(int i=0;i<vis->var_list.size();i++)
+      {
+        vis->var_list[i]->accept(this);
+      }
+    }
+    // int visit(Var_declarations *vis)
+    // {
+
+    // }
+    int visit(Var_method_declaration *vis)
+    {
+      cout<<"Entered Printing Variables\n";
+      cout<<vis->name<<"\n";
     }
 };
