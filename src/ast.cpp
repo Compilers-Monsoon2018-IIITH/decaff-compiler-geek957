66785ed413cca6128f1f951cb0aeb63c84d27ee4
rallaakhil <r.akhil.9640@gmail.com>
2018-10-31 14:20:34 +0530

    Done method declarations

--- src/ast.cpp
+++ src/ast.cpp
@@ -13,7 +13,10 @@ int  AST::accept(Visitor *v){v->visit(this);}
 // {	public:
   	///declarlist *de;
   	// statlist *se;
-Program::Program(){}
+Program::Program(Field_declarations *fields_declarations)
+{
+  this->fields = fields_declarations;
+}
  //  	Program(class fieldDeclarations *fields, class methodDeclarations *methods) {
 	//     this->methods = methods;
 	//     this->fields = fields;
@@ -26,7 +29,6 @@ Program::Program(){}
    //  	this->se=s;
 
   	// }
-int Program::accept(Visitor *v){v->visit(this);}
 // };
 
 
@@ -37,9 +39,54 @@ int Program::accept(Visitor *v){v->visit(this);}
     // interpret(vector<statlist *>s){
     //   this->sa=s;
     // }
-int DFS::visit(Program *vis)
-    {
-    	cout<<"class Program found"<<endl;
-    	// vis->accept();
-    }
+void Field_declarations::Push_back(Field_declaration *field_declaration)
+{
+  declaration_list.push_back(field_declaration);
+}
+
+
+
+
+Field_declaration::Field_declaration(string dataType, Var_declarations *var_declarations)
+{
+  this->dataType = dataType;
+  this->var_list = var_declarations->getVarsList();
+}
+
+
+
+void Var_declarations::Push_back(Var_declaration *var) {
+    var_declaration_list.push_back(var);
+}
+vector<Var_declaration *> Var_declarations::getVarsList() {
+    return var_declaration_list;
+}
+
+
+
+
+Var_declaration::Var_declaration(string name, unsigned int array_size) {
+    this->declType = variableType::Array;
+    this->name = name;
+    this->length = array_size;
+}
+Var_declaration::Var_declaration(string name) {
+    this->declType = variableType::Normal;
+    this->name = name;
+}
+
+
+
+
+
+
+// int DFS::visit(Program *vis){
+// 	cout<<"class Program found"<<endl;
+// 	// vis->accept();
+// }
+// int DFS::visit(Program *vis)
+// {
+//   cout<<"class Program found"<<endl;
+//   // vis->accept();
+// }
 // };
--- src/ast.h
+++ src/ast.h
@@ -4,11 +4,23 @@ using namespace std;
 
 class AST;
 class Program;
+class Field_declarations;
+class Field_declaration;
+class Var_declarations;
+class Var_declaration;
+
+enum variableType{
+    Array = 1, Normal = 2
+};
 class Visitor
 {
 	public:
 		virtual int visit(AST *vis){}
 		virtual int visit(Program *vis){}
+		virtual int visit(Field_declarations *vis){}
+    virtual int visit(Field_declaration *vis){}
+    // virtual int visit(Var_declarations *vis){}
+    virtual int visit(Var_declaration *vis){}
 		// virtual int visit(statlist *sta){}
 		// virtual int visit(stat *sta){}
 		// virtual int visit(bl *sta){}
@@ -29,6 +41,8 @@ class Visitor
 		// virtual int visit(ass *sta){}
 };
 
+
+
 class AST
 {
 	public:
@@ -41,7 +55,9 @@ class Program:public AST
 {	public:
   	///declarlist *de;
   	// statlist *se;
-  	Program();
+  		class Field_declarations *fields;
+  		Program(class Field_declarations *);
+
  //  	Program(class fieldDeclarations *fields, class methodDeclarations *methods) {
 	//     this->methods = methods;
 	//     this->fields = fields;
@@ -54,8 +70,66 @@ class Program:public AST
    //  	this->se=s;
 
   	// }
-  	virtual int accept(Visitor *v);
+  	virtual int accept(Visitor *v){v->visit(this);}
 };
+
+class Field_declarations:public AST
+{
+	public:
+		vector<class Field_declaration *> declaration_list;
+		Field_declarations() = default;
+		void Push_back(class Field_declaration *);
+	virtual int accept(Visitor *v){v->visit(this);}
+};
+
+
+class Field_declaration:public AST
+{
+	public:
+		string dataType;
+		// class Type *typ;
+		vector<Var_declaration *> var_list;
+		// class Var_declarations *vars;
+		Field_declaration(string, Var_declarations *);
+	virtual int accept(Visitor *v){v->visit(this);}
+};
+
+class Var_declarations:public AST
+{
+	public:
+		vector<Var_declaration *> var_declaration_list;
+		Var_declarations() = default;
+		void Push_back(Var_declaration *);
+		vector<Var_declaration *> getVarsList();
+	virtual int accept(Visitor *v){v->visit(this);}
+};
+
+class Var_declaration:public AST
+{
+	public:		
+		variableType declType;
+		unsigned int length;
+		string name;
+		Var_declaration(string, unsigned int);
+    	explicit Var_declaration(string);
+    	bool isArray() { return (declType == variableType::Array); }
+    	unsigned int getLength() {return length;}
+	virtual int accept(Visitor *v){v->visit(this);}
+};	
+
+// class Type:public AST
+// {
+// 	public:
+// 		Type(string);
+// 	virtual int accept(Visitor *v){v->visit(this);}
+// }
+
+// class Int_lit:public AST
+// {
+// 	public:
+// 		Int_lit(int);
+// 	virtual int accept(Visitor *v){v->visit(this);}
+// }
 
 
 class DFS:public Visitor
@@ -65,5 +139,44 @@ public:
     // interpret(vector<statlist *>s){
     //   this->sa=s;
     // }
-    int visit(Program *vis);
+    int visit(Program *vis)
+    {
+    	cout<<"entered Program\n";
+    	if(vis->fields!=NULL)
+    		vis->fields->accept(this);
+    }
+    int visit(Field_declarations *vis)
+    {
+    	cout<<"Entered Field_declarations\n";
+    	for(int i=0;i<vis->declaration_list.size();i++)
+    		vis->declaration_list[i]->accept(this);
+    }
+    int visit(Field_declaration *vis)
+    {
+    	cout<<"Entered Field_declaration\n";
+    	cout<<"Variable Type "<<vis->dataType<<"\n";
+    	for(int i=0;i<vis->var_list.size();i++)
+    	{
+    		vis->var_list[i]->accept(this);
+    	}
+    }
+    // int visit(Var_declarations *vis)
+    // {
+
+    // }
+    int visit(Var_declaration *vis)
+    {
+    	cout<<"Entered Printing Variables\n";
+    	if(vis->declType==1)
+    	{
+    		cout<<"It's an array\n";
+    		cout<<vis->name<<"\n";
+    		cout<<vis->length<<"\n";
+    	}
+    	else
+    	{
+			cout<<"It's a Normal variable\n";
+    		cout<<vis->name<<"\n";
+    	}
+    }
 };
